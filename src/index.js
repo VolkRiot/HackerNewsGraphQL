7fa02c68d232b3088ab5cb1177941c25c2f372f0
Mikhail Metrikin <metrikin@gmail.com>
2017-08-03 19:37:25 -0700

    Apollo provider and endpoints

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloProvider,
+  createNetworkInterface,
+  ApolloClient
+} from 'react-apollo';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj5x5446vws3r0183e6dljkgy'
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
