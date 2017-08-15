c3a1e06432e98e882ac52da9217ec05b069c4e5f
Mikhail Metrikin <metrikin@gmail.com>
2017-08-15 13:02:10 -0700

    Subscription example with Apollo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import {
   applyMiddleware
 } from 'react-apollo';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  SubscriptionClient,
+  addGraphQLSubscriptions
+} from 'subscriptions-transport-ws';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -15,6 +19,21 @@ import { GC_AUTH_TOKEN } from './constants';
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj5x5446vws3r0183e6dljkgy'
 });
+
+const wsClient = new SubscriptionClient(
+  'wss://subscriptions.graph.cool/v1/cj5x5446vws3r0183e6dljkgy',
+  {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(GC_AUTH_TOKEN)
+    }
+  }
+);
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+);
 
 networkInterface.use([
   {
@@ -30,7 +49,7 @@ networkInterface.use([
 ]);
 
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions
 });
 
 ReactDOM.render(
